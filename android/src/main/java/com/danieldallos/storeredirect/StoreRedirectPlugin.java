7fb6d95cac2243b0f37d6896b563a7012bc58cad
mirimhee <48482259+LIMMIHEE@users.noreply.github.com>
2022-07-06 11:06:30 +0900

    Update StoreRedirectPlugin.java

--- android/src/main/java/com/danieldallos/storeredirect/StoreRedirectPlugin.java
+++ android/src/main/java/com/danieldallos/storeredirect/StoreRedirectPlugin.java
@@ -63,7 +63,7 @@ public class StoreRedirectPlugin implements MethodCallHandler, FlutterPlugin, Ac
       Intent intent = new Intent(Intent.ACTION_VIEW);
       intent.setData(Uri.parse("https://play.google.com/store/apps/details?id="+appPackageName));
       intent.setPackage(appPackageName);
-      startActivity(intent);
+      this.activity.startActivity(intent);
 //       Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName));
 //       marketIntent.addFlags(
 //           Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
